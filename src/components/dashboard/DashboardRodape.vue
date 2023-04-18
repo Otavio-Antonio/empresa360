1df9eeeabe9f28275d97dd73c7f590b98aa67e52
Otavio <otavio.srds1@gmail.com>
2023-04-18 16:51:15 -0300

    Adição de rodapé especifico para a view dashboard.

---
+++ src/components/dashboard/DashboardRodape.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h5>Aqui é o rodape do dashboard</h5>
+    </div>
+</template>
+<script>
+/* eslint-disable */
+export default{
+    name: 'DashboardRodape'
+}
+</script>
